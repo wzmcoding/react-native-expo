31cccae6cdc30b90ae8ecd2ce7afee07600d7ef6
wzm <2791063637@qq.com>
2025-04-18 16:56:57 +0800

    feat: 显示选定的表情符号

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
 import EmojiPicker from '@/components/EmojiPicker';
 import EmojiList from '@/components/EmojiList';
+import EmojiSticker from '@/components/EmojiSticker';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -54,6 +55,7 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
