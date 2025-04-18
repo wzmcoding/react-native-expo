72476818b28e0e1692474803a60f98e167da1f7e
wzm <2791063637@qq.com>
2025-04-18 14:55:39 +0800

    feat: 使用 Pressable 创建按钮

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet } from 'react-native';
 import ImageViewer from "@/components/ImageViewer";
+import Button from '@/components/Button';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -8,6 +9,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -21,5 +26,10 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
