48a95b9759d933746a7ad1518a2b9e185eaf2cc5
wzm <2791063637@qq.com>
2025-04-18 16:48:04 +0800

    feat:  显示表情符号列表

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { type ImageSource } from 'expo-image';
+
 import ImageViewer from "@/components/ImageViewer";
 import Button from '@/components/Button';
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
 import EmojiPicker from '@/components/EmojiPicker';
-import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import EmojiList from '@/components/EmojiList';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -13,6 +16,7 @@ export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -66,7 +70,7 @@ export default function Index() {
         </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        {/* A list of emoji component will go here */}
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
     </View>
   );
