3a7d6aaa738ffc36496e3894a960b52693f6f0a0
wzm <2791063637@qq.com>
2025-04-18 14:44:12 +0800

    feat:  将组件拆分为文件

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View, StyleSheet } from 'react-native';
-import { Image } from 'expo-image'
+import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -7,7 +7,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
       </View>
     </View>
   );
@@ -22,9 +22,4 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  }
 });
